85a4afa0e2447953041f2ea678d066a0616819c6
Kusseler <andresmartinezbedoya@gmail.com>
2024-02-24 13:13:21 -0500

    Added the 17th exercise and the README
    Added the 17th Exercise with the snippets of code; also added the README.md to answer this questions later

---
+++ First_Unit/Programs/Seventeenth_Exercise/Seventeenth_Exercise.ino
@@ -0,0 +1,25 @@
+void task1(){
+
+  if(Serial.available() >= 3){
+    int dataRx1 = Serial.read()
+    int dataRx2 = Serial.read()
+    int dataRx3 = Serial.read()
+  }
+
+  if(Serial.available() >= 2){
+    int dataRx1 = Serial.read()
+    int dataRx2 = Serial.read()
+    int dataRx3 = Serial.read()
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
