5aeb9bdb68853fe28eae677c3508a37954a4f5f3
Andrés_Martínez <andres.martinezb@upb.edu.co>
2024-04-17 09:16:07 -0500

    Added the 3rd excercise
    Added the 3rd exercise to the 3rd unit

---
+++ Third_Unit/Arduino Programs/Exercise_3/Exercise_3.ino
@@ -0,0 +1,57 @@
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+  // 45 60 55 d5
+  // https://www.h-schmidt.net/FloatConverter/IEEE754.htmlstatic 
+  float num = 3589.3645;
+  static uint8_t arr[4] = {0};
+
+  if(Serial.available()){
+    if(Serial.read() == 's'){
+      memcpy(arr,(uint8_t *)&num,4);
+      for(int8_t i = 3; i >= 0; i--){
+        Serial.write(arr[i]);
+      }
+    }
+  }
+}
+
+// Opción 1
+
+// void setup() {
+  //Serial.begin(115200);
+//}
+
+//void loop() {
+    // 45 60 55 d5
+    // https://www.h-schmidt.net/FloatConverter/IEEE754.html
+    //static float num = 3589.3645;
+
+    //if(Serial.available()){
+        //if(Serial.read() == 's'){
+            //Serial.write ( (uint8_t *) &num,4);
+        //}
+    //}
+//}
+
+// Opción 2
+
+//void setup() {
+  //Serial.begin(115200);
+//}
+
+//void loop() {
+  // 45 60 55 d5
+  // https://www.h-schmidt.net/FloatConverter/IEEE754.htmlstatic 
+  //float num = 3589.3645;
+  //static uint8_t arr[4] = {0};
+
+  //if(Serial.available()){
+    //if(Serial.read() == 's'){
+      //memcpy(arr,(uint8_t *)&num,4);
+      //Serial.write(arr[i]);
+    //}
+  //}
+//}
---
+++ Third_Unit/Arduino Programs/Exercise_3/README.md
@@ -0,0 +1,5 @@
+# Third Exercise
+The purpose of this exercise is to understand how to send numbers to the serial. In this case, we have 4 numbers in hexadecimal and using a website, we can convert them to decimal and from there, send them to the serial.
+
+### Sending the same sequence but backwards
+The way to send the same sequence of numbers but backwards, we use a for cycle for the arragement but not in the normal ++ way that we usually do, we do -- since we starting at the 3rd position of the array and we start substracting on the positions.
